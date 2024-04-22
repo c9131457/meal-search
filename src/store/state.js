ba820389a1d0fef46d94b5b66789e62447042a91
c0131457 <c9131457@gmail.com>
2024-04-21 21:17:53 -0500

    data store and meals by letter complete

---
+++ src/store/state.js
@@ -0,0 +1,8 @@
+// import MealsByIngredients from "@/views/MealsByIngredients.vue";
+
+export default {
+    searchedMeals: [],
+    mealsByLetter: [],
+    MealsByIngredient: [],
+    ingredient: {}
+}
--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -1,5 +1,20 @@
 <script setup>
 
+import { ref, onMounted, computed } from 'vue'
+import useAPI from '@/composables/useAPI';
+
+const { api } = useAPI()
+
+const keyword = ref('')
+const ingredients = ref([])
+
+onMounted(() => {
+    api.get('list.php?i=list')
+        .then(({ data }) => {
+        ingredients.value = data.meals
+    })
+})
+
 </script>
 
 <template>
