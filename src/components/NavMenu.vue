4f2e1c2576078330b71df8efc0539aa5bde604ba
c0131457 <c9131457@gmail.com>
2024-04-24 16:58:37 -0500

    Fix Error two

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -2,10 +2,10 @@
 import HomePage from '@/views/HomePage.vue'
 <template>
   <nav>
-    <RouterLink to="{name: 'Home'}">Home</RouterLink>
-    <RouterLink to="{name: 'MealsByName'}">Meals By Name</RouterLink>
-    <RouterLink to="{name: 'MealsByLetter'}">Meals By Letter</RouterLink>
-    <RouterLink to="{name: 'Ingredients'}">Meals By Ingredients</RouterLink>
+    <RouterLink :to="{name: 'Home'}">Home</RouterLink>
+    <RouterLink :to="{name: 'MealsByName'}">Meals By Name</RouterLink>
+    <RouterLink :to="{name: 'MealsByLetter'}">Meals By Letter</RouterLink>
+    <RouterLink :to="{name: 'Ingredients'}">Meals By Ingredients</RouterLink>
   </nav>
 </template>
 
