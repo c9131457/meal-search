46a7d31f8b8d112ab5b0bde8990f8b0a9e8660ab
c0131457 <c9131457@gmail.com>
2024-04-28 11:16:05 -0500

    Getting Ingredients

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -18,5 +18,9 @@ onMounted(() => {
 </script>
 
 <template>
-    
+    <div class="p-8 pb-0 text-orange-500">
+        <h1 class="text-4xl font-bold mb-4">Search Meals By Ingredients</h1>
+    </div>
+
+    {{ ingredients }}
 </template>
